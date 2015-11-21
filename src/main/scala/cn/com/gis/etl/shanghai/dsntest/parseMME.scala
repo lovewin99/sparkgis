af2dfaba4b41a17f417ef0677678f0e9508df19b
vlem <vlem@163.com>
2015-11-21 13:36:57 +0800

    10161  buildinginfo

---
+++ src/main/scala/cn/com/gis/etl/shanghai/dsntest/parseMME.scala
@@ -0,0 +1,56 @@
+package cn.com.gis.etl.shanghai.dsntest
+
+import java.util.regex.Pattern
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * Created by wangxy on 15-8-6.
+ */
+object parseMME {   // 列分隔符
+  private val mmeSep = Pattern.compile("\\|")                                      // 列分隔符
+
+  def main(args: Array[String]):Unit={
+    if (args.length != 2) {
+      System.err.println("Usage: <in-file> <out-file>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+
+    val mmeFile = sc.textFile("dsn/s1ap")
+    val mmeData = mmeFile.map{
+      _ match {
+        case mme(mmeUserId, mmeGroupId, mmeCode, startTime, "1") => (("","",""),("",""))
+        case mme(mmeUserId, mmeGroupId, mmeCode, startTime, imsi) => ((mmeUserId, mmeGroupId,mmeCode), (startTime, imsi))
+        case _ => (("","",""),("",""))
+      }
+    }.filter(_ != (("","",""),("","")))
+
+    val sortData = mmeData.groupByKey(100).mapPartitions{ iter =>
+      iter.map{
+        case ((mmeUserId, mmeGroupId,mmeCode), values) =>
+          val temp = values.toList.sortBy(_._1).last
+          Seq(mmeUserId, mmeGroupId,mmeCode, temp).mkString(",")
+      }
+    }
+
+    sortData.repartition(50).saveAsTextFile("dsn/mmeoutput")
+
+
+  }
+
+  // object mme
+  object mme{
+    def unapply(str: String) = {
+      val arr = mmeSep.split(str)
+      arr.length match {
+          // mmeues1apid mmegroupid mmecode starttime imsi
+        case 169 => Some(arr(18), arr(22), arr(23), arr(9), arr(5))
+        case _   => None
+      }
+    }
+  }
+}
+
+
---
+++ src/main/scala/cn/com/gis/etl/shanghai/dsntest/parseXmlDsnTest.scala
@@ -0,0 +1,109 @@
+package cn.com.gis.etl.shanghai.dsntest
+
+import java.util.regex.Pattern
+
+import cn.com.utils.XmlInputFormat
+import com.utils.ConfigUtils
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Created by wangxy on 15-8-6.
+ */
+object parseXmlDsnTest {
+  private val conf = ConfigUtils.getConfig("/config/dsn.dsntest.properties")  // 配置文件
+  private val allColumnsLab = conf.getOrElse("all.columns","")                // 所有列定义
+  private val clos = conf.getOrElse("value.columns","")                    // 所需字段列
+  private val sep = Pattern.compile(" ")                                      // 列分隔符
+  private val mmeSep = Pattern.compile("\\|")                                      // 列分隔符
+  val indexArray = getValueColumnIndex()
+
+  def parseXML1(content: String) ={
+    val a = xml.XML.loadString(content)
+    // <measurement>...</measurement>
+    // and smr == allColumnsLab
+    val c = (a \\ "measurement").filter(x => (x \ "smr" text) == allColumnsLab) \\ "object"
+    c.map{ x =>
+      val mmeId      = (x \ "@MmeUeS1apId" text)
+      val mmeGroupId = (x \ "@MmeGroupId" text)
+      val mmeCode    = (x \ "@MmeCode" text)
+      val timeValue  = (x \ "@TimeStamp" text).replaceAll("(\\d{4}-\\d{2}-\\d{2})T(\\d{2}:\\d{2}:\\d{2})\\.\\d+", "$1 $2")  // timestamp
+      val eNBId      = (x \ "@id" text).split(":", 2)(0)
+
+      val ret2 = ArrayBuffer[String]()
+      ret2 += (mmeId, mmeGroupId, mmeCode, timeValue, eNBId)
+      val ret  = ArrayBuffer[String]()
+      (x \ "v").foreach{ x =>
+        val tempColumns = sep.split((x text).trim)
+        val aa = indexArray.map(tempColumns(_))
+        ret.clear()
+        ret += (aa(0), aa(1))
+        if (aa(2) != "NIL" && aa(3) != "NIL")
+          ret2 += aa(2) + "|" + aa(3)
+      }
+      if (ret(1) != "NIL") {
+        ret2 patch (5, ret, 5)
+        val arr = ret2.splitAt(3)
+        (arr._1.mkString(","), arr._2.mkString(","))
+      } else
+        ("","")
+    }
+  }
+
+  // 返回所需列的索引
+  def getValueColumnIndex() ={
+    val allMap = sep.split(allColumnsLab).zipWithIndex.toMap
+    sep.split(clos).map(allMap.getOrElse(_, 0))
+  }
+
+  def main(args: Array[String]):Unit={
+    if (args.length != 2) {
+      System.err.println("Usage: <in-file> <out-file>")
+      System.exit(1)
+    }
+
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+    val Array(srcPath, tarPath) = args
+
+    sc.hadoopConfiguration.set("xmlinput.start", "<bulkPmMrDataFile>")
+    sc.hadoopConfiguration.set("xmlinput.end", "</bulkPmMrDataFile>")
+
+
+    val mrData = sc.newAPIHadoopFile[LongWritable, Text, XmlInputFormat](srcPath).mapPartitions{x =>
+      x.flatMap{y =>parseXML1(y._2.toString)}
+    }.filter{
+      case ("","") => true
+      case _ => false
+    }
+
+      //.saveAsTextFile(tarPath)
+
+    val mmeFile = sc.textFile("dsn/s1ap")
+    val mmeData = mmeFile.map{
+      _ match {
+        case mme(mmeUserId, mmeGroupId, mmeCode, imsi) => (Seq(mmeUserId, mmeGroupId,mmeCode).mkString(","), imsi)
+        case _ => ("", "")
+      }
+    }
+
+    val result = mrData.join(mmeData).saveAsTextFile(tarPath)
+
+  }
+
+
+  // object mme
+  object mme{
+    def unapply(str: String) = {
+      val arr = mmeSep.split(str)
+      arr.length match {
+        case 169 => Some(arr(18), arr(22), arr(23), arr(5))
+        case _   => None
+      }
+    }
+  }
+}
+
+
